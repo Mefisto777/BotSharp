72709764525f795176e7532a027ad22807a95731
Mefisto777 <pesoc397@gmail.com>
2022-08-02 22:19:49 +0300

    Добавьте файлы проекта.

---
+++ BotSharp/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Ecng.Serialization;
+using StockSharp.Algo;
+using StockSharp.Configuration;
+using System.IO;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace BotSharp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (File.Exists(fileName))
+            connector.Load(new XmlSerializer<SettingsStorage>().Deserialize(fileName));
+        }
+        Connector connector = new Connector();
+        string fileName = "Connect";
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            connector.Configure(this);
+            new XmlSerializer<SettingsStorage>().Serialize(connector.Save(), fileName);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            connector.Connect();
+        }
+    }
+}
